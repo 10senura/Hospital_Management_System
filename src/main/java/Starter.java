031a556b597846cc49cb627a7863f4d972e03c58
SENURA INDEEWARA <senuraindeewara@gmail.com>
2025-01-10 20:30:39 +0530

    first commit

---
+++ src/main/java/Starter.java
@@ -0,0 +1,16 @@
+import com.sun.javafx.scene.control.FakeFocusTextField;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Starter extends Application {
+    public static void main(String[] args) {
+        launch();
+    }
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("view/dash_board_form_comtroller.fxml"))));
+        stage.show();
+    }
+}
