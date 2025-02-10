f68e606ccb7285281a221fdbe0587c9b014d280d
SENURA INDEEWARA <senuraindeewara@gmail.com>
2025-02-10 23:05:25 +0530

    create ui?ux

--- src/main/java/Controller/DashBoardFormController.java
+++ src/main/java/Controller/DashBoardFormController.java
@@ -1,20 +1,15 @@
 package Controller;
 
-import Controller.Model.Patient;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
-import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
 
 import java.io.IOException;
 import java.net.URL;
-import java.util.ResourceBundle;
 
 public class DashBoardFormController {
 
@@ -44,6 +39,8 @@ public class DashBoardFormController {
 
     @FXML
     private TableView <?> tblPatientView;
+
+
 
     @FXML
     void btnAppointmentOnAction(ActionEvent event) throws IOException {
@@ -87,7 +84,7 @@ public class DashBoardFormController {
 
     @FXML
     void btnPatientsOnAction(ActionEvent event) throws IOException {
-        URL resource = this.getClass().getResource("/view/patient_form_controller.fxml");
+        URL resource = this.getClass().getResource("/view/patient_form_view_controller.fxml");
         assert resource!=null;
         Parent lode = FXMLLoader.load(resource);
         this.lodeFormController.getChildren().clear();
--- src/main/java/Controller/DashboardView.java
+++ src/main/java/Controller/DashboardView.java
@@ -1,4 +1,18 @@
 package Controller;
 
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.AnchorPane;
+
 public class DashboardView {
+    
+    public AnchorPane lodeFormController;
+    public TableView tblPatientView;
+    public TableColumn clmPatientId;
+    public TableColumn clmPatientName;
+    public TableColumn clmPatientAge;
+    public TableColumn clmPatientGender;
+    public TableColumn clmPatientContact;
+    public TableColumn clmPatientEmergencyContact;
+    public TableColumn clmPatientMedicalHistory;
 }
